bf2297550edbd89dbbfaa8d56a6fd40f7056e7bc
Ryan <rrs305@gmail.com>
2021-02-22 22:32:29 -0800

    2/22/21 Java project page 59

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,19 @@
+window.alert("Hello World!"); //Creating a pop up window
+document.write("Hello, World!"); //Puts statement on webpage
+var A = "Lisa told Bart, \"Knock it off, Bart!; Or I'll tell dad!" //Created a variable also shows how to include quotes in a string
+document.write(A); //Putting statement (A) on webpage
+var b = "Feb 22" + " 2021"; //assinging a variable
+window.alert(b); //creating an alert window 
+document.write("\"Be who you are and say what you feel." +
+    " because those who mind don\'t matter and those who matter don\'t mind.\"" +
+    "-Dr. Seuss"); //how to add strings together along with how to use \
+var Family = "The Squires",
+    Dad = "Ryan",
+    Mom = "Danielle",
+    Daughter = "Catelyn",
+    Son = "Cody"; //creating multiple variables
+document.write(Mom); //print a variable
+var blues = "I have the blues.";
+var blues = blues.fontcolor("blue"); //changing variable font color
+document.write(blues); //printing a variable in color
+document.write(3 + 3); //printing an expression
