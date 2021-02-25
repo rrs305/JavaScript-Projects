ccd8b3ab755b2e09621c697948601b9ab169a031
Ryan <rrs305@gmail.com>
2021-02-24 18:29:54 -0800

    Update main.js
    code fix from page 59
    concatenating 2 variables

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -2,8 +2,9 @@ window.alert("Hello World!"); //Creating a pop up window
 document.write("Hello, World!"); //Puts statement on webpage
 var A = "Lisa told Bart, \"Knock it off, Bart!; Or I'll tell dad!" //Created a variable also shows how to include quotes in a string
 document.write(A); //Putting statement (A) on webpage
-var b = "Feb 22" + " 2021"; //assinging a variable
-window.alert(b); //creating an alert window 
+var b = "Feb 22"; //assinging a variable
+var c = " 2021"; //assinging a second variable
+window.alert(b + c); //creating an alert window 
 document.write("\"Be who you are and say what you feel." +
     " because those who mind don\'t matter and those who matter don\'t mind.\"" +
     "-Dr. Seuss"); //how to add strings together along with how to use \
